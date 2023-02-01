8331198c703d9ffa4c93ef55f6ebe531a2eca5ef
khyojun <nandong5923@naver.com>
2023-02-02 01:57:48 +0900

    feat: update관련 객체 작성

--- src/main/java/org/springboot/service/posts/PostsService.java
+++ src/main/java/org/springboot/service/posts/PostsService.java
@@ -3,8 +3,11 @@ package org.springboot.service.posts;
 
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springboot.domain.posts.Posts;
 import org.springboot.domain.posts.PostsRepository;
+import org.springboot.web.dto.PostsResponseDto;
 import org.springboot.web.dto.PostsSaveRequestDto;
+import org.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -16,6 +19,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 
 
