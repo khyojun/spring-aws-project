1d015156bea13a0273a2a9e47b66220cfe0e9e01
khyojun <nandong5923@naver.com>
2023-02-02 22:21:07 +0900

    feat: update기능 추가

--- src/main/java/org/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/springboot/domain/posts/PostsRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
 
-    @Query("SELECT p FROM Posts p order by p.id DESC")
+    @Query("SELECT p FROM Posts p order by p.id ASC")
     List<Posts> findAllDesc();
 }
