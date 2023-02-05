372eeb5ac060ffb367a60157f780951810917ad5
khyojun <nandong5923@naver.com>
2023-02-05 23:48:39 +0900

    feat: annotation활용하여 코드 바꿔보기

--- src/main/java/org/springboot/web/IndexController.java
+++ src/main/java/org/springboot/web/IndexController.java
@@ -2,6 +2,7 @@ package org.springboot.web;
 
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springboot.config.auth.LoginUser;
 import org.springboot.config.auth.dto.SessionUser;
 import org.springboot.service.posts.PostsService;
 import org.springboot.web.dto.PostsResponseDto;
@@ -18,10 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user !=null){
             model.addAttribute("user", user.getName());
