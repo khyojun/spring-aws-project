5ffece6a5550e5a670e77131562ef114a8b995fd
khyojun <nandong5923@naver.com>
2023-02-06 00:53:46 +0900

    feat : naver에 대한 oauth 설정 코드 작성

--- src/main/java/org/springboot/config/WebConfig.java
+++ src/main/java/org/springboot/config/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(loginUserArgumentResolver)
+        resolvers.add(loginUserArgumentResolver);
     }
 }
--- src/main/java/org/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/org/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,7 +28,23 @@ public class OAuthAttributes {
 
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributes, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder().
+            name((String) response.get("name"))
+            .email((String) response.get("email"))
+            .picture((String) response.get("profile_image"))
+            .attributes(response).
+            nameAttributeKey(userNameAttributes).
+            build();
     }
 
 
