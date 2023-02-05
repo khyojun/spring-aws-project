634a02a5237a66b8a27cc3d2b069e23d1498bd57
khyojun <nandong5923@naver.com>
2023-02-05 23:15:41 +0900

    feat: 직렬화 관련 주석 추가

--- src/main/java/org/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/org/springboot/config/auth/dto/SessionUser.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 import org.springboot.domain.user.User;
 
 @Getter
-public class SessionUser implements Serializable {
+public class SessionUser implements Serializable  // 직렬화(Serialization)란 자바 시스템 내에서 사용하는 객체 또는 데이터를 자바시스템 외에서도 사용할 수 있도록 Byte 형태로 데이터를 변환하는 기술이다.(파일로 만든다고 보면된다.)
+{
 
     private String name;
     private String email;
