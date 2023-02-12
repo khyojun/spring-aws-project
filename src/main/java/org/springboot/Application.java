c4e7e0dbf9a4d4bba2d34a78a5735cda213c74b4
khyojun <nandong5923@naver.com>
2023-02-12 19:47:30 +0900

    feat : 자동 배포를 위한 설정 추가

--- src/main/java/org/springboot/Application.java
+++ src/main/java/org/springboot/Application.java
@@ -3,6 +3,7 @@ package org.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
     }
 }
