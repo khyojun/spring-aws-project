70993a0b94364939fe78f5d28063ed861fe8ac9a
khyojun <nandong5923@naver.com>
2023-02-12 20:13:46 +0900

    code 수정

--- src/main/java/org/springboot/Application.java
+++ src/main/java/org/springboot/Application.java
@@ -3,7 +3,6 @@ package org.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -11,10 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static void main(String[] args) {
-
-        SpringApplication application = new SpringApplication(Application.class);
-        application.addListeners(new ApplicationPidFileWriter());
-        application.run(args);
-
+        SpringApplication.run(Application.class, args);
     }
 }
